7ee89151a303f4f16be4004518b0eef50ee7e9b8
Alexander Gurin <alexandergurin1996@gmail.com>
2017-04-14 00:04:49 +0300

    Изменил объект "Pers"

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-class Pers {
-    var name;
-    var class;
-    var hp;
-    var xp;
-}
+var Pers = {
+    name: 'default',
+    clas: 'default',
+    hp: 100,
+    xp: 0
+};
 
 document.addEventListener("DOMContentLoaded", {
     //document.getElementById("header").setAttribute("", "");
