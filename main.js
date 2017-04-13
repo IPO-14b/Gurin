211083b1b2e4256fd54fbd4ca79690b7614eb760
Alexander Gurin <alexandergurin1996@gmail.com>
2017-04-13 23:45:46 +0300

    Создал класс "Pers"(Персонаж)

---
+++ main.js
@@ -0,0 +1,10 @@
+class Pers {
+    var name;
+    var class;
+    var hp;
+    var xp;
+}
+
+document.addEventListener("DOMContentLoaded", {
+    //document.getElementById("header").setAttribute("", "");
+});
